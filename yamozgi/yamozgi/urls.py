c14d2ff970213e67ac7ed638510d8bb3360074d5
vanger <vanger2607@gmailcom>
2022-12-14 20:41:03 +0300

    small fixes-2

--- yamozgi/yamozgi/urls.py
+++ yamozgi/yamozgi/urls.py
@@ -3,7 +3,10 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("homepage.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("homepage.urls")),
+]
 urlpatterns += static(
     settings.STATIC_URL,
     document_root=settings.STATIC_ROOT,
