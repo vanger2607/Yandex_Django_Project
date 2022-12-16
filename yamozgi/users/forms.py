7578f7e9bd12a84896c195eed36481e5963e6612
ksusha <ktsykanova@yandex.ru>
2022-12-16 22:38:19 +0300

    create sign in form

--- yamozgi/users/forms.py
+++ yamozgi/users/forms.py
@@ -3,9 +3,9 @@ from django import forms
 from .models import CustomUser
 
 
-class SignupForm(forms.ModelForm):
+class SignUpForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
-        super(SignupForm, self).__init__(*args, **kwargs)
+        super(SignUpForm, self).__init__(*args, **kwargs)
         self.fields["password_confirm"] = forms.CharField(
             max_length=128,
             required=True,
@@ -28,8 +28,10 @@ class SignupForm(forms.ModelForm):
         }
 
         widgets = {
-            mail: forms.EmailInput(attrs={"class": "form-control", "required": True}),
-            login: forms.TextInput(attrs={"class": "form-control", "required": True}),
+            mail: forms.EmailInput(attrs={"class": "form-control",
+                                          "required": True}),
+            login: forms.TextInput(attrs={"class": "form-control",
+                                          "required": True}),
             password: forms.PasswordInput(
                 attrs={"class": "form-control", "required": True}
             ),
@@ -40,7 +42,46 @@ class SignupForm(forms.ModelForm):
     }
 
     def clean_password_confirm(self):
-        cleaned_data = super(SignupForm, self).clean()
+        cleaned_data = super(SignUpForm, self).clean()
+        password = cleaned_data.get("password")
+        password_confirm = cleaned_data.get("password_confirm")
+
+        if password != password_confirm:
+            raise forms.ValidationError(
+                self.error_messages["password_mismatch"],
+                code="password_mismatch",
+            )
+        return cleaned_data
+
+
+class SignInForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(SignInForm, self).__init__(*args, **kwargs)
+        self.fields["login_or_mail"] = forms.CharField(
+            required=True,
+            label="Логин или почта",
+            widget=forms.PasswordInput(attrs={"class": "form-control",
+                                              "required": True}),
+        )
+
+    class Meta:
+        model = CustomUser
+        password = CustomUser.password.field.name
+
+        fields = (password, )
+
+        labels = {
+            password: "Пароль",
+        }
+
+        widgets = {
+            password: forms.PasswordInput(
+                attrs={"class": "form-control", "required": True}
+            ),
+        }
+
+    def clean_password_confirm(self):
+        cleaned_data = super(SignInForm, self).clean()
         password = cleaned_data.get("password")
         password_confirm = cleaned_data.get("password_confirm")
 
