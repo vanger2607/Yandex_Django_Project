71ce4ebc8cc693c527dab476ca3eaa2d9bc5913c
ksusha <ktsykanova@yandex.ru>
2022-12-16 22:38:32 +0300

    create sign in url

--- yamozgi/users/urls.py
+++ yamozgi/users/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path, re_path
+from django.urls import path
 
 from . import views
 
 app_name = "users"
 urlpatterns = [
     path("signup/", views.sign_up, name="signup"),
+    path("signin/", views.sign_in, name="signin"),
 ]
