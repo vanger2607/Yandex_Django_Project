c7c24e82ea25cf994e1b9869ed2c333033d1980c
vanger <vanger2607@gmailcom>
2022-12-16 14:54:41 +0300

    small fixes

--- yamozgi/leaderboard/apps.py
+++ yamozgi/leaderboard/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class LeaderboardConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'leaderboard'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "leaderboard"
--- yamozgi/yamozgi/settings.py
+++ yamozgi/yamozgi/settings.py
@@ -26,7 +26,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
     "homepage.apps.HomepageConfig",
     "questions.apps.QuestionsConfig",
     "duels.apps.DuelsConfig",
