d3810492c862854571a31749d8776e0cf5f1fd0f
ksusha <ktsykanova@yandex.ru>
2022-12-21 23:34:34 +0300

    update models

--- yamozgi/duels/models.py
+++ yamozgi/duels/models.py
@@ -5,6 +5,23 @@ from users.models import CustomUser
 from arena.models import Category
 
 
+class Challenge(models.Model):
+    player_sent_id = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name="player_sent",
+    )
+    player_recieved_id = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name="player_recieved",
+    )
+
+    class Meta:
+        verbose_name = "вызов"
+        verbose_name_plural = "вызовы"
+
+
 class Battle(models.Model):
     player_1 = models.ForeignKey(
         CustomUser,
@@ -16,30 +33,33 @@ class Battle(models.Model):
         on_delete=models.CASCADE,
         related_name="battle_player2",
     )
-    number_of_rounds = models.PositiveSmallIntegerField()
-    is_over = models.BooleanField()
+    is_over = models.BooleanField(default=0)
 
     class Meta:
-        verbose_name = "поединок"
-        verbose_name_plural = "поединки"
+        verbose_name = "битва"
+        verbose_name_plural = "битвы"
 
 
 class Round(models.Model):
-    player_first = models.ForeignKey(
-        CustomUser,
-        on_delete=models.CASCADE,
-        related_name="first_turn_player",
-    )
-    player_second = models.ForeignKey(
-        CustomUser,
-        on_delete=models.CASCADE,
-        related_name="second_turn_player",
-    )
+    battle_id = models.ForeignKey(Battle, on_delete=models.CASCADE)
+    is_over = models.BooleanField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    questions = models.ManyToManyField(Question)
-    is_over = models.BooleanField()
-    battle = models.ForeignKey(Battle, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = "раунд"
         verbose_name_plural = "раунды"
+
+
+class Player_answer(models.Model):
+    player_id = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name="battle_player",
+    )
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    round_id = models.ForeignKey(Round, on_delete=models.CASCADE)
+    is_right = models.BooleanField(null=True)
+
+    class Meta:
+        verbose_name = "вопрос в раунде"
+        verbose_name_plural = "вопросы в раунде"
