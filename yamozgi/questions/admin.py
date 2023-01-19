133cc1838740db5a1d13b54473f19750a096f7b7
vanger <vanger2607@gmailcom>
2023-01-19 17:41:01 +0300

    improve a little admin

--- yamozgi/questions/admin.py
+++ yamozgi/questions/admin.py
@@ -5,7 +5,7 @@ from questions.models import Category, Question
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ("question_text", "category", "is_approved", "author_id")
+    list_display = ("question_text", "category", "is_approved", "author_id",)
     list_filter = ("is_approved", "author_id",)
     list_editable = ("is_approved",)
     list_display_links = ("author_id",)
